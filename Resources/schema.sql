9270e2b863c42fa79e11e5b3da1d351a6a5f6fef
paozmoreno <102872511+paozmoreno@users.noreply.github.com>
2022-08-07 12:14:39 -0700

    Add files via upload

--- Resources/schema.sql
+++ Resources/schema.sql
@@ -1,51 +1,3 @@
-DROP TABLE population_2019
-
-CREATE TABLE population_2019(
-	row INT PRIMARY KEY,
-	country VARCHAR(50),
-	year INTEGER,
-	population INTEGER,
-	male_population INTEGER,
-	female_population INTEGER,
-	annual_growth_rate FLOAT
-);
-
-DROP TABLE population_2020
-
-CREATE TABLE population_2020(
-	row INT PRIMARY KEY,
-	country VARCHAR(50),
-	year INTEGER,
-	population INTEGER,
-	male_population INTEGER,
-	female_population INTEGER,
-	annual_growth_rate FLOAT
-);
-
-DROP TABLE population_2021
-
-CREATE TABLE population_2021(
-	row INT PRIMARY KEY,
-	country VARCHAR(50),
-	year INTEGER,
-	population INTEGER,
-	male_population INTEGER,
-	female_population INTEGER,
-	annual_growth_rate FLOAT
-);
-
-DROP TABLE population_2022
-
-CREATE TABLE population_2022(
-	row INT PRIMARY KEY,
-	country VARCHAR(50),
-	year INTEGER,
-	population INTEGER,
-	male_population INTEGER,
-	female_population INTEGER,
-	annual_growth_rate FLOAT
-);
-
 DROP TABLE global_data
 
 CREATE TABLE global_data(
@@ -58,4 +10,16 @@ CREATE TABLE global_data(
 	total_deaths INTEGER,
 	deaths_in_last_7days INTEGER,
 	deaths_in_last_24hrs INTEGER
+);
+
+DROP TABLE timeline_data
+;
+
+CREATE TABLE timeline_data(
+	id SERIAL,
+	country VARCHAR(50),
+	date VARCHAR (50),
+	confirmed_cases INTEGER,
+	confirmed_deaths INTEGER,
+	population INTEGER
 );
